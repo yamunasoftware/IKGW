4ad4017562a423f0f84ff6baeef23b12a15fbbd9
Sushen Kolakaleti <yamunasoftware18@gmail.com>
2026-02-09 18:25:09 -0500

    Updated Start Script

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-apt-get update && apt-get install -y ca-certificates openjdk-17-jre-headless
+apt-get update && apt-get install -y openjdk-17-jre-headless
 mkdir -p /ikgw/logs
 mvn clean package
 java -jar target/IKGW-0.0.1-SNAPSHOT.jar
