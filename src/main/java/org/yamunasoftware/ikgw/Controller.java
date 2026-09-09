44b56ded8944c3a8e4a51083764a299a98d8d68e
Sushen Kolakaleti <yamunasoftware18@gmail.com>
2026-09-09 18:57:00 -0400

    Updated Controller Dependency Injection

--- src/main/java/org/yamunasoftware/ikgw/Controller.java
+++ src/main/java/org/yamunasoftware/ikgw/Controller.java
@@ -1,6 +1,7 @@
 package org.yamunasoftware.ikgw;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,21 +11,19 @@ import org.slf4j.LoggerFactory;
 @RestController
 @RequestMapping("/ikgw/api")
 public class Controller {
+  private final String kafkaTopic;
   private final Logger logger = LoggerFactory.getLogger(Controller.class);
   private final KafkaTemplate<String, SensorReading> kafkaTemplate;
 
   @Autowired
-  public Controller(KafkaTemplate<String, SensorReading> kafkaTemplate) {
+  public Controller(KafkaTemplate<String, SensorReading> kafkaTemplate, @Value("KAFKA_TOPIC") String kafkaTopic) {
     this.kafkaTemplate = kafkaTemplate;
+    this.kafkaTopic = kafkaTopic;
   }
 
   @PostMapping("publish")
   public void publishMessage(@RequestBody SensorReading reading) {
-    try {
-      String topic = System.getenv("KAFKA_TOPIC");
-      kafkaTemplate.send(topic, reading);
-    }
-
+    try { kafkaTemplate.send(kafkaTopic, reading); }
     catch (Exception e) {
       logger.error("Unexpected Error in POST /ikgw/api/publish", e);
     }
