b702c860c2c74f809e731a75903654c054eb5fc4
Sushen Kolakaleti <yamunasoftware18@gmail.com>
2026-09-10 20:06:31 -0400

    Added Health Endpoint

--- src/main/java/org/yamunasoftware/ikgw/Controller.java
+++ src/main/java/org/yamunasoftware/ikgw/Controller.java
@@ -2,8 +2,15 @@ package org.yamunasoftware.ikgw;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,15 +29,29 @@ public class Controller {
     this.kafkaTopic = kafkaTopic;
   }
 
+  @GetMapping("health")
+  public ResponseEntity<String> health() {
+    try {
+      return new  ResponseEntity<>(HttpStatus.OK);
+    }
+
+    catch (Exception e) {
+      logger.error("Unexpected Error in GET /ikgw/api/health", e);
+      return new  ResponseEntity<>("Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
   @PostMapping("publish")
-  public void publishMessage(@RequestBody SensorReading reading) {
+  public ResponseEntity<String> publishMessage(@RequestBody SensorReading reading) {
     try {
       SensorReadingMessage message = new SensorReadingMessage(reading);
       kafkaTemplate.send(kafkaTopic, message);
+      return new ResponseEntity<>(HttpStatus.OK);
     }
 
     catch (Exception e) {
       logger.error("Unexpected Error in POST /ikgw/api/publish", e);
+      return new  ResponseEntity<>("Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 }
